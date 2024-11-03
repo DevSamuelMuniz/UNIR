61a07d980f83e3321f9fa7266a97aeb6b2bb8af1
DevSamuelMuniz <devsamuelmuniz@hotmail.com>
2024-11-03 02:31:04 -0300

    fix bug again and again and again

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
-    domains: ['localhost', 'https://672703342a603e72a78041da--unir.netlify.app/'],  
   },
   reactStrictMode: true,
 };
